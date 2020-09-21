3f783196d122141bb31379713f77aafcc19cfa30
EugeneZakhno <zohan1234567@gmail.com>
2020-09-21 21:15:05 +0300

    Commit sorting

---
+++ src/main/java/com/example/java8exmp/Developer.java
@@ -0,0 +1,27 @@
+package com.example.java8exmp;
+
+import java.math.BigDecimal;
+
+public class Developer {
+    String name;
+    int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Developer(String mkyong, BigDecimal bigDecimal, int i) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
