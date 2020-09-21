5e8f4b5d32054e6db384feacaa6424d02289b3d8
EugeneZakhno <zohan1234567@gmail.com>
2020-09-21 21:08:15 +0300

    init

---
+++ src/main/java/com/example/java8exmp/Java8exmpApplication.java
@@ -0,0 +1,13 @@
+package com.example.java8exmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8exmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8exmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java8exmp/Java8exmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java8exmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8exmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
